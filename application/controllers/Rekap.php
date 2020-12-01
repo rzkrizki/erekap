dae12ad6e924b5170bb7041cbb97ec9e7d26ea28
Taufiq Hidayat <taufiqhidayat1611.th@gmail.com>
2020-12-01 10:37:18 +0700

    rekap data from api

--- application/controllers/Rekap.php
+++ application/controllers/Rekap.php
@@ -8,9 +8,8 @@ class Rekap extends CI_Controller {
         parent::__construct();
         date_default_timezone_set('Asia/Jakarta');
         $this->load->model('Dashboard_model');
+        $this->load->model('api_model', 'api');
         
-        // $this->load->library('datatables');
-
         $data = $this->session->userdata('teknopol');
         if(!$data){
           redirect('');
@@ -27,7 +26,7 @@ class Rekap extends CI_Controller {
 		$id_provinsi = $this->db->where('id', $user['kab_id'])->get('m_regencies')->row('province_id');
 		
 		$data['provinsi']	= $this->db->where('id', $id_provinsi)->get('m_provinces')->result();
-		
+		$data['id_kabupaten'] = $user['kab_id'];
         $data['flashdata'] = 0;
 
     	$this->load->view('template2/header');	
@@ -49,6 +48,33 @@ class Rekap extends CI_Controller {
 	function get_tree_kelurahan($id){
 		$data = $this->Dashboard_model->get_tree('kelurahan', $id);
 		echo json_encode($data);
+	}
+    
+    function filter_data(){
+
+		$post = $this->input->post();
+		
+		$token_id = 'bf1cdad49abdbb8ed400c1de3d776f2c';
+		$id_kabupaten = $post['id_kabupaten'];
+        $id_kecamatan = $post['id_kecamatan'];
+		$id_kelurahan = $post['id_kelurahan'];
+		
+
+        $url = "https://api.digipol.id/api/get_erekap_paslon?token=".$token_id."&id_kecamatan=".$id_kecamatan."&id_kelurahan=".$id_kelurahan."&id_kabupaten=".$id_kabupaten."";
+        $token = "f91b12d9-d5d6-40aa-8d66-d04e70a6c08b";
+
+        $response = $this->api->get_dynamic_data($url, $token);
+        //print_r($response);
+        if($response->error == "false"){
+            
+            $data['status'] = 'success';
+			$data['result'] = $response->data;
+            $data['summary'] = $response->summary;
+        }else{
+            $data['status'] = 'error';
+            $data['message'] = $response->message;
+        }
+        echo json_encode($data);
 	}
 	
 }
